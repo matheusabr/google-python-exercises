09b6d508acaec5727abac197d7d5cb59850f0da5
Matheus Abreu <matheusabreu.dev@gmail.com>
2017-10-27 22:12:51 -0300

    refactor: match_ends

--- basic/list1.py
+++ basic/list1.py
@@ -24,9 +24,8 @@
 def match_ends(words):
     count = 0
     for word in words:
-        if len(word) >= 2:
-            if (word[0] == word[-1]):
-                count += 1
+        if len(word) >= 2 and (word[0] == word[-1]):
+            count += 1
     return count
 
 
